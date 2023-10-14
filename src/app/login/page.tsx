e04173c9410636b1da1a24147f9e3bbb05c3215b
shreya <shetshreyaa25@gmail.com>
2023-10-14 22:03:52 +0530

    chore: connect to db

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import {axios} from "axios";
+import axios from "axios";
 
 
 function Page() {
